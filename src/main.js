6c60b1200bc307869c72fdf508e527bb5b823b1a
developerjin <devjin0617@gmail.com>
2019-04-06 01:17:54 +0900

    at-ui 추가

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import AtComponents from 'at-ui'
+
+Vue.use(AtComponents)
 
 Vue.config.productionTip = false
 
